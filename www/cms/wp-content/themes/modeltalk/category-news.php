ed6215e4d9885bc17c1ac4c162dc604ecff17be0
Eric <eric@xtunes.cc>
2011-04-24 16:57:05 +0800

    修改分页代码

--- www/cms/wp-content/themes/modeltalk/category-news.php
+++ www/cms/wp-content/themes/modeltalk/category-news.php
@@ -8,17 +8,14 @@ get_header(); ?>
 			<div id="content">
 				<div class="main">
 				<ul  class="newslist">
-					<?php $my_query = new WP_Query( array('category_name' => news , 'posts_per_page'=>5 ,'paged' => get_query_var('paged') ) ); ?>
-					<?php if ( $my_query->have_posts() ) : while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 					<li>				
 						<h1><a href="<?php the_permalink() ?>" target="_top"><?php the_title(); ?></a></h1>
 					</li>
 					   <?php endwhile; endif; ?>
 		</ul>
 				<div class="pagination">
-			<?php if(function_exists('wp_pagenavi')) { wp_pagenavi( array( 'query' => $my_query )); } 
-			wp_reset_postdata();
-			?>
+			<?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } ?>
 		</div>
 				</div>
 			</div>
--- www/cms/wp-content/themes/modeltalk/category-products.php
+++ www/cms/wp-content/themes/modeltalk/category-products.php
@@ -8,13 +8,15 @@ get_header(); ?>
 			<div id="content">
 				<div class="main">
 				<ul  class="productlist">
-					<?php $posts = query_posts($query_string . '&orderby=date&showposts=-1'); ?>
 					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 					<li><a href="<?php the_permalink() ?>" target="_top"><?php the_post_thumbnail('thumbnail');?></a>				
 						<h1><a href="<?php the_permalink() ?>" target="_top"><?php the_title(); ?></a></h1>
 					</li>
 					   <?php endwhile; endif; ?>
 		</ul>
+						<div class="pagination">
+			<?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } ?>
+		</div>
 				</div>
 			</div>
 <?php get_footer(); ?>
